58e3a05fe57a0831288cabf7765f828501fb5c7e
Rashid Khan <rashood.khan@gmail.com>
2014-06-25 13:28:28 +0400

    Initial commit with blog, events and filemanager model

---
+++ blog/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.auth.models import  User
+import os
+
+# Create your models here.
+
+def get_upload_path(self,filename):
+    return os.path.join("files_uploaded", filename)
+
+class Blog(models.Model):
+    title = models.CharField(max_length=50)
+    date_posted = models.DateTimeField('Posted Date',auto_now_add=True)
+    content = models.TextField()
+    author = models.ForeignKey(User)
+
+    def __unicode__(self):
+        return self.title
+
+class Event(models.Model):
+    name = models.CharField(max_length=50)
+    start_date = models.DateTimeField('Start Time',auto_now_add=True)
+    end_date = models.DateField('End Time')
+    venue = models.CharField(max_length=50)
+
+    def __unicode__(self):
+        return self.name
+
+class FileManager(models.Model):
+    title = models.CharField(max_length=50)
+    CATEGORY = (
+        ('Audio','Audio'),
+        ('Video','Video'),
+        ('Document','Document')
+    )
+    category = models.CharField('File Category', choices=CATEGORY, max_length=50)
+    file_uploaded = models.FileField(upload_to=get_upload_path)
